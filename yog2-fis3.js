f46497765a0d9e133549de358081727bd57eb8f3
hefangshi <hefangshi@gmail.com>
2016-06-23 20:03:21 +0800

    support client node_modules

--- yog2-fis3.js
+++ yog2-fis3.js
@@ -1,7 +1,6 @@
 'use strict';
 
 var fis = module.exports = require('fis3');
-var path = require('path');
 fis.require.prefixes.unshift('yog2');
 fis.require.prefixes.unshift('yogurt');
 fis.cli.name = 'yog2';
@@ -25,54 +24,55 @@ fis.set('project.ignore', [
     'build.sh',
     'component.json',
     'output/**',
+    '/client/node_modules/**',
     'fis-conf.js'
 ]);
 
 var clientRoadmap = {
     // all release to $static dir
-    'client/(**)': {
+    '/client/(**)': {
         id: '$1',
         moduleId: '${namespace}:$1',
         release: '/${static}/${namespace}/$1'
     },
-    'client/**.less': {
+    '/client/**.less': {
         parser: fis.plugin('less'),
         rExt: '.css'
     },
-    'client/{**.ts,**.tsx,**.jsx,**.es}': {
+    '/client/{**.ts,**.tsx,**.jsx,**.es}': {
         parser: fis.plugin('typescript', {
             module: 1,
             target: 0
         }),
         rExt: 'js'
     },
-    'client/**.tpl': {
+    '/client/**.tpl': {
         preprocessor: fis.plugin('extlang'),
         postprocessor: fis.plugin('require-async'),
         useMap: true
     },
-    'client/**.{tpl,js,ts,jsx,es,tsx}': {
+    '/client/**.{tpl,js,ts,jsx,es,tsx}': {
         useSameNameRequire: true
     },
-    'client/page/**.tpl': {
+    '/client/page/**.tpl': {
         extras: {
             isPage: true
         }
     },
-    'client/(page/**.tpl)': {
+    '/client/(page/**.tpl)': {
         url: '${namespace}/$1',
         release: '/${template}/${namespace}/$1',
         useMap: true
     },
-    'client/(widget/**.tpl)': {
+    '/client/(widget/**.tpl)': {
         url: '${namespace}/$1',
         release: '/${template}/${namespace}/$1',
         useMap: true
     },
-    'client/{components,widget}/**.{js,es,ts,tsx,jsx,css,less}': {
+    '/client/{components,widget}/**.{js,es,ts,tsx,jsx,css,less}': {
         isMod: true
     },
-    'client/test/(**)': {
+    '/client/test/(**)': {
         useMap: false,
         release: '/test/${namespace}/$1'
     },
@@ -92,7 +92,7 @@ var commonRoadmap = {
 };
 
 var serverRoadmap = {
-    'server/(**)': {
+    '/server/(**)': {
         useMap: false,
         preprocessor: false,
         standard: false,
@@ -103,47 +103,87 @@ var serverRoadmap = {
         isMod: false,
         release: '${app}/${namespace}/$1'
     },
-    'server/{**.ts,**.es}': {
+    '/server/{**.ts,**.es}': {
         parser: fis.plugin('typescript', {
             module: 1,
             target: 2
         }),
         rExt: 'js'
     },
-    '{node_modules/**,package.json}': {
+    '/{node_modules/**,package.json}': {
         useCompile: false,
         release: 'app/${namespace}/$0'
     }
 };
 
 var prodRoadmap = {
-    'client/**.{js,css,less,ts,jsx,es,tsx}': {
+    '/client/**.{js,css,less,ts,jsx,es,tsx}': {
         useHash: true
     },
-    'client/**.{js,ts,jsx,es,tsx}': {
+    '/client/**.{js,ts,jsx,es,tsx}': {
         optimizer: fis.plugin('uglify-js')
     },
-    'client/**.{css,less}': {
+    '/client/**.{css,less}': {
         optimizer: fis.plugin('clean-css')
     },
     '::image': {
         useHash: true
     },
-    'client/**.png': {
+    '/client/**.png': {
         optimizer: fis.plugin('png-compressor')
     }
 };
 
 // 添加自定义命令
-
 fis.require._cache['command-run'] = require('./command/run.js');
 fis.require._cache['command-util'] = require('./command/util.js');
 
-[commonRoadmap, clientRoadmap, serverRoadmap, prodRoadmap].forEach(function (roadmap) {
-    fis.util.map(roadmap, function (selector, rules) {
+[commonRoadmap, clientRoadmap, serverRoadmap, prodRoadmap].forEach(function(roadmap) {
+    fis.util.map(roadmap, function(selector, rules) {
         fis.match(selector, rules);
     });
 });
+
+fis.enableNPM = function(options) {
+    fis.match('/client/node_modules/**.js', {
+        isMod: true
+    });
+    if (options.autoPack) {
+        fis.match('/client/node_modules/**.js', {
+            packTo: options.npmBundlePath || '/client/pkg/npm/bundle.js'
+        });
+        fis.match('/client/node_modules/**.css', {
+            packTo: options.npmCssBundlePath || '/client/pkg/npm/bundle.css'
+        });
+        fis.on('deploy:start', function(groups) {
+            groups.forEach(function(group) {
+                var modified = group.modified;
+                var total = group.modified;
+                var file;
+                var i = modified.length - 1;
+                while ((file = modified[i--])) {
+                    if (file.subpath.indexOf('/client/node_modules') === 0) {
+                        modified.splice(i + 1, 1);
+                    }
+                }
+                i = total.length - 1;
+                while ((file = total[i--])) {
+                    if (file.subpath.indexOf('/client/node_modules') === 0) {
+                        total.splice(i + 1, 1);
+                    }
+                }
+            });
+        });
+    }
+    fis.match('/client/**.{js,es,jsx,ts,tsx}', {
+        preprocessor: [
+            fis.plugin('js-require-file'),
+            fis.plugin('js-require-css')
+        ]
+    });
+    fis.unhook('components');
+    fis.hook('node_modules');
+};
 
 // 模块化支持
 fis.hook('commonjs', {
